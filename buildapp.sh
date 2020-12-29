64988c5ae6c0de5d4768c631db41d868fbba223e
Nick Adams <nickolas.j.adams@gmail.com>
2020-12-29 13:03:48 -0700

    modified build process and added zip

--- buildapp.sh
+++ buildapp.sh
@@ -13,17 +13,24 @@ if [[ "$#" -eq 1 ]]; then
 		printUsage
 
 	elif [[ "$1" == "clean" ]]; then
-		rm -rf build dist Scraper.zip
+		rm -rf build dist
 
 	elif [[ "$1" == "test" ]]; then
 		python3 ./setup.py py2app -A
+
+	elif [[ "$1" == "final" ]] || [[ "$1" == "prod" ]]; then
+		python3 setup.py py2app
 
 	elif [[ "$1" == "zip" ]] || [[ "$1" == "dist" ]] || [[ "$1" == "distribution" ]] || [[ "$1" == "archive" ]]; then
 		ditto -c -k --sequesterRsrc --keepParent dist Scraper.zip
 	fi
 
 elif [[ "$#" -eq 0 ]]; then
-	python3 setup.py py2app
+
+	./buildapp.sh final
+	./buildapp.sh zip
+	./buildapp.sh clean
+
 else
 	printUsage
 fi
